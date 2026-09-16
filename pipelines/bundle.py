89e2a047abce7970224fdd13d4cda8675d356110
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2026-09-16 12:23:37 +0200

    Use custom single pass PMTiles writer (#318)

--- pipelines/bundle.py
+++ pipelines/bundle.py
@@ -4,11 +4,12 @@ import time
 import sys
 import json
 import os
+from io import BytesIO
 
 import mercantile
-from pmtiles.tile import zxy_to_tileid, TileType, Compression
-from pmtiles.reader import Reader, MmapSource, all_tiles
-from pmtiles.writer import Writer
+from pmtiles.tile import TileType, Compression, deserialize_header, deserialize_directory, Entry
+from pmtiles.reader import Reader, MmapSource
+from pmtiles.writer import Writer, finalize_header
 
 import utils
 
@@ -60,103 +61,126 @@ def get_dirty_parents(num_aggregations):
 
     return list(dirty_parents)
 
-def read_full_archive(filepath):
-    tile_id_to_bytes = {}
-    with open(filepath , 'r+b') as f2:
-        reader = Reader(MmapSource(f2))
-        for tile_tuple, tile_bytes in all_tiles(reader.get_bytes):
-            tile_id = zxy_to_tileid(*tile_tuple)
-            tile_id_to_bytes[tile_id] = tile_bytes
-    return tile_id_to_bytes
+def traverse_dry_run(get_bytes, header, dir_offset, dir_length):
+    entries = deserialize_directory(get_bytes(dir_offset, dir_length))
+    for entry in entries:
+        if entry.run_length > 0:
+            for i in range(entry.run_length):
+                yield entry.tile_id + i, header['tile_data_offset'] + entry.offset, entry.length
+        else:
+            for t in traverse_dry_run(
+                get_bytes,
+                header,
+                header['leaf_directory_offset'] + entry.offset,
+                entry.length,
+            ):
+                yield t
 
+def all_tiles_dry_run(get_bytes):
+    header = deserialize_header(get_bytes(0, 127))
+    return traverse_dry_run(get_bytes, header, header['root_offset'], header['root_length'])
+    
 def create_archive(filepaths, name):
+    print(f'start working on {name}...')
+
     utils.create_folder('bundle-store')
     out_filepath = f'bundle-store/{name}.pmtiles'
     checksum = None
+
     min_z = math.inf
     max_z = 0
     min_lon = math.inf
     min_lat = math.inf
     max_lon = -math.inf
     max_lat = -math.inf
-    with open(out_filepath, 'wb') as f1:
-        hash_writer = utils.HashWriter(f1)
-        writer = Writer(hash_writer)
-        
-        tile_ids_and_filepaths = []
+    tiles_filepaths = []
+    tile_data_length = 0
+    print('dry run over input tiles...')
+    for filepath in filepaths:
+        filename = filepath.split('/')[-1]
+        z, x, y, child_z = [int(a) for a in filename.replace('.pmtiles', '').split('-')]    
+        max_z = max(max_z, child_z)
+        min_z = min(min_z, child_z)
+        west, south, east, north = mercantile.bounds(x, y, z)
+        min_lon = min(min_lon, west)
+        min_lat = min(min_lat, south)
+        max_lon = max(max_lon, east)
+        max_lat = max(max_lat, north)
 
-        j = 0
-        for filepath in filepaths:
-            filename = filepath.split('/')[-1]
-            z, x, y, child_z = [int(a) for a in filename.replace('.pmtiles', '').split('-')]
-            parent = mercantile.Tile(x=x, y=y, z=z)
-            tiles = []
-            if z == child_z:
-                tiles.append(parent)
-            else:
-                tiles += mercantile.children(parent, zoom=child_z)
-            for tile in tiles:
-                tile_id = zxy_to_tileid(tile.z, tile.x, tile.y)
-                tile_ids_and_filepaths.append((tile_id, filepath))
-        
-            max_z = max(max_z, child_z)
-            min_z = min(min_z, child_z)
-            west, south, east, north = mercantile.bounds(x, y, z)
-            min_lon = min(min_lon, west)
-            min_lat = min(min_lat, south)
-            max_lon = max(max_lon, east)
-            max_lat = max(max_lat, north)
-            j += 1
-            if j % 1000 == 0:
-                print(f'prepared {j:_} / {len(filepaths):_} filepaths...')
+        with open(filepath , 'r+b') as f:
+            reader = Reader(MmapSource(f))
+            for tile_id, tile_offset, tile_length in all_tiles_dry_run(reader.get_bytes):
+                tiles_filepaths.append((tile_id, tile_offset, tile_length, filepath))
+                tile_data_length += tile_length
+    
+    print(f'found {len(tiles_filepaths):_} tiles ({(tile_data_length / 1024 ** 3):.2f} GiB)')
 
-        tile_ids_and_filepaths = sorted(tile_ids_and_filepaths)
-        
+    tiles_filepaths.sort()
+
+    tile_entries = []
+    offset = 0
+    for tile_id, _, tile_length, __ in tiles_filepaths:
+        tile_entries.append(Entry(tile_id, offset, tile_length, 1))
+        offset += tile_length
+
+    min_lon_e7 = int(min_lon * 1e7)
+    min_lat_e7 = int(min_lat * 1e7)
+    max_lon_e7 = int(max_lon * 1e7)
+    max_lat_e7 = int(max_lat * 1e7)
+
+    header = {
+        'tile_type': TileType.WEBP,
+        'tile_compression': Compression.NONE,
+        'min_zoom': min_z,
+        'max_zoom': max_z,
+        'min_lon_e7': min_lon_e7,
+        'min_lat_e7': min_lat_e7,
+        'max_lon_e7': max_lon_e7,
+        'max_lat_e7': max_lat_e7,
+        'center_zoom': int(0.5 * (min_z + max_z)),
+        'center_lon_e7': int(0.5 * (min_lon_e7 + max_lon_e7)),
+        'center_lat_e7': int(0.5 * (min_lat_e7 + max_lat_e7)),
+        'encoding': 'terrarium',
+    }
+    addressed_tiles_count  = len(tile_entries)
+    tile_contents_count = len(tile_entries)
+    metadata = {
+        'attribution': '<a href="https://mapterhorn.com/attribution">© Mapterhorn</a>',
+    }
+    clustered = True
+    header_bytes, root_bytes, compressed_metadata, leaves_bytes = finalize_header(header, addressed_tiles_count, tile_entries, tile_contents_count, metadata, clustered, tile_data_length)
+
+    with open(out_filepath, 'wb') as f:
+
+        print('writing header...')
+        hash_writer = utils.HashWriter(f)
+        hash_writer.write(header_bytes)
+        hash_writer.write(root_bytes)
+        hash_writer.write(compressed_metadata)
+        hash_writer.write(leaves_bytes)
+
         last_filepath = None
-        tile_id_to_bytes = None
+        in_pmtiles_data = None
 
-        j = 0
-        start = time.time()
-        for tile_id, filepath in tile_ids_and_filepaths:
-            if filepath != last_filepath:
+        print('writing tiles...')
+
+        last_percentage = None
+        bytes_written = 0
+        for _, tile_offset, tile_length, filepath in tiles_filepaths:
+            if last_filepath != filepath:
                 last_filepath = filepath
-                tile_id_to_bytes = read_full_archive(filepath)
-            writer.write_tile(tile_id, tile_id_to_bytes[tile_id])
+                with open(filepath, 'rb') as f_in:
+                    in_pmtiles_data = f_in.read()
+            hash_writer.write(in_pmtiles_data[tile_offset:(tile_offset + tile_length)])
+            bytes_written += tile_length
+            percentage = int(100 * bytes_written / tile_data_length)
+            if last_percentage != percentage:
+                print(f'{name}.pmtiles: wrote {percentage} % ({(bytes_written / 1024 ** 3):.2f} GiB / {(tile_data_length / 1024 ** 3):.2f} GiB)')
+                last_percentage = percentage
 
-            j += 1
-            if j % 10_000 == 0:
-                tic = time.time()
-                time_so_far = tic - start
-                expected_duration = time_so_far * len(tile_ids_and_filepaths) / j
-                finishes_in = expected_duration - time_so_far
-                print(f'Processed {j:_} / {len(tile_ids_and_filepaths):_} tiles in {int(time_so_far / 60)} min {int(time_so_far) % 60} s. Finishes in {int(finishes_in / 3600)} h {int(finishes_in / 60) % 60} min...')
-
-        min_lon_e7 = int(min_lon * 1e7)
-        min_lat_e7 = int(min_lat * 1e7)
-        max_lon_e7 = int(max_lon * 1e7)
-        max_lat_e7 = int(max_lat * 1e7)
-
-        writer.finalize(
-            {
-                'tile_type': TileType.WEBP,
-                'tile_compression': Compression.NONE,
-                'min_zoom': min_z,
-                'max_zoom': max_z,
-                'min_lon_e7': min_lon_e7,
-                'min_lat_e7': min_lat_e7,
-                'max_lon_e7': max_lon_e7,
-                'max_lat_e7': max_lat_e7,
-                'center_zoom': int(0.5 * (min_z + max_z)),
-                'center_lon_e7': int(0.5 * (min_lon_e7 + max_lon_e7)),
-                'center_lat_e7': int(0.5 * (min_lat_e7 + max_lat_e7)),
-                'encoding': 'terrarium',
-            },
-            {
-                'attribution': '<a href="https://mapterhorn.com/attribution">© Mapterhorn</a>',
-            },
-        )
         checksum = hash_writer.md5.hexdigest()
-
+        
+    print(checksum)
     utils.create_folder('meta-store/bundle')
     filesize = os.path.getsize(out_filepath)
     with open(f'meta-store/bundle/{name}.json', 'w') as f:
